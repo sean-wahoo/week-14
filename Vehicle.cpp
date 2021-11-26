5ee5a31cbde6934bbe283f3993b65d5f2ac55ffa
= <=>
2021-11-26 03:33:39 -0500

    defined functions for vehicle class

---
+++ Vehicle.cpp
@@ -0,0 +1,20 @@
+#include "Vehicle.h"
+#include <iomanip>
+#include <iostream>
+
+string Vehicle::getMan() const
+{
+    return manufacturer;
+}
+
+int Vehicle::getYear() const
+{
+    return year;
+}
+
+void Vehicle::displayInfo()
+{
+    cout << "=== VEHICLE INFO ===" << endl;
+    cout << "Manufacturer: " << manufacturer << endl;
+    cout << "Year built: " << year << endl;
+}
